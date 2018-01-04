9a7cf0bb0584d1269e149b9343df2a28729b6f88
atulyapradhan <admin@admin-PC>
2018-01-04 11:42:11 +0530

    Commit the changes

---
+++ src/test/java/mavendemo/SampleTest.java
@@ -0,0 +1,16 @@
+package mavendemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void simpleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a+b == 4);
+		
+	}
+
+}
